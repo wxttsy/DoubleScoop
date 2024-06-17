a9267788e91d3f7acf64db0a4d674590dc967364
Michael-JBourke <145739872+Michael-JBourke@users.noreply.github.com>
2024-06-17 16:57:32 +1000

    Interacting With scene
    Ice cream scoop grab working

---
+++ Assets/DoubleScoopAssets/Scripts/IceCreamScooper.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCreamScooper : MonoBehaviour
+{
+    //private var inputController = new List<UnityEngine.XR.InputDevice>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

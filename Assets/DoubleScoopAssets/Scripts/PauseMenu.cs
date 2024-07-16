c8c563b41422176e8314092c1d26e52db1c11357
MazzaMatress <132528980+MazzaMatress@users.noreply.github.com>
2024-07-16 11:56:42 +1000

    PauseMenu Complete
    controller button still needs to be assigned

--- Assets/DoubleScoopAssets/Scripts/PauseMenu.cs
+++ Assets/DoubleScoopAssets/Scripts/PauseMenu.cs
@@ -1,18 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject PauseScreen;
+    [SerializeField] GameObject SettingsScreen;
+    [SerializeField] GameObject ControlsScreen;
+    bool isPaused = false;
+    private void Awake()
     {
-        
+        PauseScreen.SetActive(false);
+        SettingsScreen.SetActive(false);
+        ControlsScreen.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TogglePauseScreen()
     {
-        
+        PauseScreen.SetActive(!PauseScreen.activeSelf);
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
     }
+
+    public void OpenSettingsScreen()
+    {
+        SettingsScreen.SetActive(true);
+        PauseScreen.SetActive(false);
+    }
+    public void CloseSettingsScreen()
+    {
+        SettingsScreen.SetActive(false);
+        PauseScreen.SetActive(true);
+    }
+
+    public void OpenControlsScreen()
+    {
+        ControlsScreen.SetActive(true);
+        PauseScreen.SetActive(false);
+    }
+    public void CloseControlsScreen()
+    {
+        ControlsScreen.SetActive(false);
+        PauseScreen.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("GameHasQuit");
+    }
+
+
 }
+
